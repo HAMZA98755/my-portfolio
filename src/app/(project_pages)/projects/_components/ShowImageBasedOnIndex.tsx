d6a9ec96a30b6591896ec7829b8a70bdf2c2c7f6
HAMZA98755 <h37003338@gmail.com>
2026-02-17 18:25:20 +0300

    Fix lazy loading of images problem

--- src/app/(project_pages)/projects/_components/ShowImageBasedOnIndex.tsx
+++ src/app/(project_pages)/projects/_components/ShowImageBasedOnIndex.tsx
@@ -1,3 +1,4 @@
+'use client'
 // Component: single-project carousel with prev/next arrows
 import Image from 'next/image'
 import {useState} from 'react'
@@ -6,71 +7,93 @@ import { useTheme } from '@/src/_contexts/ThemeContext';
 import type { Locale } from '@/src/_lib/translations';
 import { ProjectImage } from '@/src/_types/Project';
 import { useLang } from '@/src/_contexts/LanguageContext';
+import { Themes } from '@/src/_ui/Themes';
 export default function ShowImageBasedOnIndex({ images}: {images: ProjectImage[]}) {
   const [idx, setIdx] = useState(0); // This State Store Index Of Current Image Will be Showed
-  const {colors} = useTheme(); 
+  const [loading, setLoading] = useState(true);
+  const {theme, colors} = useTheme(); 
+  const t = Themes[theme];
   const {lang} = useLang();
   const isArabic = lang === 'ar'
+  type ArrowDirection = 'left' | 'right'
 
   const numberOfImages = images?.length || 0; // Return Number of Images
   if (numberOfImages === 0) return null; 
 
   const next = () => setIdx((i) => (i + 1) % numberOfImages); // This Function Change Current Image to Next Image
   const prev = () => setIdx((i) => (i - 1 + numberOfImages) % numberOfImages);// This Function Change Current Image to Previous Image
+  
+
+
+  function Arrow ({dir}: {dir: ArrowDirection}) {
+    
+    return (
+      <div className={'flex align-center justify-center '}>
+        { numberOfImages > 1 && (  
+          <button
+            onClick={dir === 'left' ? isArabic ?  next : prev : isArabic ?  prev : next}
+            style={{color: t.text}}
+            >
+            {dir === 'left' ? <IoIosArrowBack size={25}/> : <IoIosArrowForward size={25}/> }
+          </button>
+        )}
+      </div>  
+    )
+  }
 
 
   return (
-    <div
-    className="relative rounded-lg mb-6"
-    style={{ borderColor: colors.border }}
-    >
-      {/* This Div Return The Current Image  */}
-      <div className="relative w-full h-64 bg-gray-100">
-        <Image
-          src={images[idx].imageUrl}
-          alt={images[idx].imageAlt}
-          fill
-          style={{ objectFit: "contain", objectPosition: "center" }}
-        />
-      </div>
+    <div style={{ borderColor: colors.border }}>
+      
+      {/* This div containe the left and right arrow and images   */}
+      <div className={'flex align-center justify-center'}
+        style={{flexDirection: isArabic ? 'row-reverse' : 'row'}}
+      >
 
-      {
-        numberOfImages > 1 && ( //  This Enshure That Number Of Images > 1 To Show The Arrows and The Reboon 
-          <>
-            {/* Button to Pass to Next Image */}
-            <button
-                onClick={isArabic ? next: prev}
-                className="absolute left-2  top-1/2 -translate-y-1/2 bg-black/40 text-white p-2 rounded-full"
-                >
-              <IoIosArrowBack/>
-            </button>
-            {/* Button to Pass to Previous Image  */}
-            <button
-              onClick={isArabic ? prev : next}
-              className="absolute right-2 top-1/2 -translate-y-1/2 bg-black/40 text-white p-2 rounded-full"
-            > 
-            <IoIosArrowForward/>
-            </button>
-            {/* Rebbon to Tell to User The Current Image  */}
-            <div className='flex justify-center absolute left-0 right-0 -bottom-3 pointer-events-none'>
-              <div className="flex gap-2 w-[80%] max-w-xl pointer-events-auto">
-                {
-                  Array.from({length: numberOfImages}).map((_, i) => (
-                    <div
-                      key={i}
-                      className= {` h-1 rounded-xl`}
-                      style={{width: `${100 / numberOfImages}%`, backgroundColor: `${ i === idx ? '#3080ff' : colors.text}`}}
-                      >
-                    </div>
-                  ))
-                }
-              </div>
+
+        {/* Button to Pass to Next Image */}
+        <Arrow dir='left'/>  
+
+        {/* Image Wraper  */}
+        <div className='relative h-64 bg-gray-100 w-[80%] mx-auto'>
+          {loading && (
+            <div className="absolute top-0 left-0 place-holder w-full h-full bg-gray-400 z-100">
             </div>
-          </>
-        )
-      }
+          )}
+          <Image
+            key={images[idx].imageUrl}
+            src={images[idx].imageUrl}
+            alt={images[idx].imageAlt}
+            fill
+            style={{ objectFit: "contain", objectPosition: "center" }}
+            onLoadingComplete={() => setLoading(false)}
+          />
+        </div>
 
-    </div>
 
+
+        {/* Button to Pass to Next Image */}
+        <Arrow dir='right'/>
+
+          
+      </div>
+    
+      { numberOfImages > 1 && (
+          <div className='flex justify-center pointer-events-none my-4 w-[90%] mx-auto'>
+          <div className="flex gap-2 w-[80%] max-w-xl pointer-events-auto">
+            {
+              Array.from({length: numberOfImages}).map((_, i) => (
+                <div
+                  key={i}
+                  className= {` h-1 rounded-xl`}
+                  style={{width: `${100 / numberOfImages}%`, backgroundColor: `${ i === idx ? '#3080ff' : colors.text}`}}
+                  >
+                </div>
+              ))
+            }
+          </div>
+      </div>
+      )}
+  </div>
   );
 }
